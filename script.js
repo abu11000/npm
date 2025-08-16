e9309c574c4809ed1b2401a1e4188b0fe5b09871
abu11000 <1337legend2000@gmail.com>
2025-08-17 00:16:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+    let h1 = document.querySelector('h1');
+    let p = document.querySelector('p');
+
+
+
+    new Typed(h1, {  
+        strings: ['Text Gradients'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        startDelay: 1000,
+        loop: false,
+    });
+    new Typed(p, {
+        strings: ['Make your headlines stand out'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        startDelay: 1000,
+        loop: false,
+    });
